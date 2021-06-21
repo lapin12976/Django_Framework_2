ea8886dc78b129cbe9d43712d38ca2254a31b29b
Danil lapin <danil129766@gmail.com>
2021-06-21 18:12:23 +0300

    first commit

---
+++ django_3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/olegmaslov/work/python/geekbrains/src_lesson_1/step_1(adapt_to_django_2.0)/geekshop/django_3/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
